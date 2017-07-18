8f14fa3d8e0f6add015a701752358fc3f14f5d10
mic277203@126.com <mic277203@126.com>
2017-07-18 10:23:41 +0800

    增加配置工具

---
+++ GuardAngelWin/Form1.Designer.cs
@@ -0,0 +1,133 @@
+﻿namespace GuardAngelWin
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.btnRefresh = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(1, 4);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersVisible = false;
+            this.dataGridView1.RowTemplate.Height = 23;
+            this.dataGridView1.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dataGridView1.Size = new System.Drawing.Size(300, 383);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // btnRefresh
+            // 
+            this.btnRefresh.Location = new System.Drawing.Point(112, 393);
+            this.btnRefresh.Name = "btnRefresh";
+            this.btnRefresh.Size = new System.Drawing.Size(75, 23);
+            this.btnRefresh.TabIndex = 1;
+            this.btnRefresh.Text = "刷新";
+            this.btnRefresh.UseVisualStyleBackColor = true;
+            this.btnRefresh.Click += new System.EventHandler(this.btnRefresh_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(308, 173);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "==>";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // dataGridView2
+            // 
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.Location = new System.Drawing.Point(389, 5);
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.RowHeadersVisible = false;
+            this.dataGridView2.RowTemplate.Height = 23;
+            this.dataGridView2.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dataGridView2.Size = new System.Drawing.Size(253, 383);
+            this.dataGridView2.TabIndex = 3;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(480, 393);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "清空";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(310, 415);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "确定";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(645, 451);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.dataGridView2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.btnRefresh);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "守护服务配置";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button btnRefresh;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataGridView dataGridView2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ GuardAngelWin/Form1.cs
@@ -0,0 +1,124 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.ServiceProcess;
+using System.Text;
+using System.Windows.Forms;
+
+namespace GuardAngelWin
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            ServiceBind();
+
+        }
+
+        private void ServiceBind()
+        {
+            DataGridViewCheckBoxColumn chk = new DataGridViewCheckBoxColumn();
+            dataGridView1.Columns.Add(chk);
+            dataGridView1.Columns[0].Width = 30;
+            var allServices = GetCurrentServices();
+            dataGridView1.DataSource = allServices;
+            dataGridView1.Columns[1].HeaderText = "名称";
+            dataGridView1.Columns[1].Width = 150;
+            dataGridView1.Columns[2].HeaderText = "描述";
+            dataGridView1.Columns[2].Width = 100;
+        }
+
+        private List<ServiceInfo> GetCurrentServices()
+        {
+            var allServices = ServiceController.GetServices();
+
+            List<ServiceInfo> listSI = new List<ServiceInfo>();
+
+            foreach (var s in allServices)
+            {
+                listSI.Add(new ServiceInfo()
+                {
+                    Name = s.ServiceName,
+                    Desc = s.DisplayName
+                });
+            }
+
+            return listSI;
+        }
+
+        private void btnRefresh_Click(object sender, EventArgs e)
+        {
+            dataGridView1.Columns.Clear();
+            ServiceBind();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            List<ServiceInfo> listSi = new List<ServiceInfo>();
+
+            foreach (var d in dataGridView1.Rows)
+            {
+                var cells = ((DataGridViewRow)d).Cells;
+
+                if (Convert.ToBoolean(cells[0].Value))
+                {
+                    listSi.Add(new ServiceInfo()
+                    {
+                        Name = cells[1].Value.ToString(),
+                        Desc = cells[2].Value.ToString()
+                    });
+                }
+            }
+
+            dataGridView2.DataSource = listSi;
+
+            dataGridView2.Columns[0].HeaderText = "名称";
+            dataGridView2.Columns[0].Width = 150;
+            dataGridView2.Columns[1].HeaderText = "描述";
+            dataGridView2.Columns[1].Width = 100;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            dataGridView2.Columns.Clear();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            List<ServiceInfo> listSi = new List<ServiceInfo>();
+
+            foreach (var d in dataGridView2.Rows)
+            {
+                var cells = ((DataGridViewRow)d).Cells;
+
+                listSi.Add(new ServiceInfo()
+                {
+                    Name = cells[0].Value.ToString(),
+                    Desc = cells[1].Value.ToString()
+                });
+            }
+
+            string content = JsonConvert.SerializeObject(listSi);
+            string jsonPath = AppDomain.CurrentDomain.BaseDirectory + "Service.json";
+
+            File.WriteAllText(jsonPath, content);
+
+            MessageBox.Show("配置成功");
+        }
+    }
+
+    public class ServiceInfo
+    {
+        public string Name { get; set; }
+        public string Desc { get; set; }
+    }
+}
