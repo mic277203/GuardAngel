a511e1b9593f14a263e7a84eec46b8f0015560c7
mic277203@126.com <mic277203@126.com>
2017-07-20 10:01:32 +0800

    增加日志，增加特殊情况的判断

--- GuardAngel/Program.cs
+++ GuardAngel/Program.cs
@@ -7,6 +7,9 @@ using System.Text;
 using System.Configuration;
 using System.Timers;
 using System.Runtime.InteropServices;
+using NLog;
+using System.Management;
+using System.Diagnostics;
 
 namespace GuardAngel
 {
@@ -16,6 +19,7 @@ namespace GuardAngel
         static extern bool ShowWindow(IntPtr hWnd, uint nCmdShow);
         [DllImport("user32.dll", EntryPoint = "FindWindow", SetLastError = true)]
         public static extern IntPtr FindWindow(string lpClassName, string lpWindowName);
+        private static readonly ILogger _logger = LogManager.GetCurrentClassLogger();
         static void Main(string[] args)
         {
             Console.Title = "GuardAngel";
@@ -36,6 +40,7 @@ namespace GuardAngel
             }
             catch (Exception ex)
             {
+                _logger.Error("读取检测频率错误，请检查配置文件是否配置");
                 throw ex;
             }
 
@@ -43,22 +48,113 @@ namespace GuardAngel
 
             time.Elapsed += (sender, e) =>
             {
+                time.Stop();
+
                 foreach (var service in allServices)
                 {
                     if (IsExists(service.ServiceName, needGuardServices))
                     {
-                        if (service.Status == ServiceControllerStatus.Stopped || service.Status == ServiceControllerStatus.StopPending)
+                        string processName = string.Empty;
+
+                        if (IsAbnormalService(service, out processName))
                         {
-                            service.WaitForStatus(ServiceControllerStatus.Stopped);
-                            service.Start();
+                            if (!string.IsNullOrEmpty(processName))
+                            {
+                                KillProcess(processName);
+                            }
+                            else
+                            {
+                                service.Refresh();
+                            }
+                        }
+
+                        if (service.Status == ServiceControllerStatus.Stopped ||
+                        service.Status == ServiceControllerStatus.StopPending)
+                        {
+                            try
+                            {
+                                service.WaitForStatus(ServiceControllerStatus.Stopped);
+                                service.Start();
+
+                                service.WaitForStatus(ServiceControllerStatus.Running);
+
+                                _logger.Info("成功启动了：{0}服务", service.ServiceName);
+                            }
+                            catch (Exception ex)
+                            {
+                                _logger.Error("启动服务:{0}错误", service.ServiceName, ex);
+                            }
                         }
                     }
                 }
+
+                time.Start();
             };
 
             time.Start();
 
             System.Threading.Thread.Sleep(-1);
+        }
+
+        static bool IsAbnormalService(ServiceController sc, out string processName)
+        {
+            ManagementObject service = new ManagementObject(@"Win32_service.Name='" + sc.ServiceName + "'");
+            object o = service.GetPropertyValue("ProcessId");
+            int processId = (int)((uint)o);
+
+            Process process = Process.GetProcessById(processId);
+
+            if (processId != 0)
+            {
+                processName = process.ProcessName;
+            }
+            else
+            {
+                processName = string.Empty;
+            }
+
+            if (sc.Status != ServiceControllerStatus.Stopped)
+            {
+                if (processId == 0)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            else
+            {
+                if (processId == 0)
+                {
+                    return false;
+                }
+                else
+                {
+                    return true;
+                }
+            }
+        }
+
+        static void KillProcess(string processName)
+        {
+            var arrProcess = Process.GetProcessesByName(processName);
+
+            for (int i = 0; i < arrProcess.Length; i++)
+            {
+                var process = arrProcess[i];
+
+                try
+                {
+                    process.Kill();
+                }
+                catch (Exception ex)
+                {
+                    _logger.Error("杀死进程:{0} 失败", process.ProcessName, ex);
+                    throw;
+                }
+            }
         }
         /// <summary>
         /// 进程名称是否存在数组中
